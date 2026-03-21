d01dce4eb213fdf6a3a08cd06d0cd89a0c1bf57c
abh1ck <abhishekkumaramba89@gmail.com>
2026-03-21 22:52:26 +0530

    Day 01 - Overlapping Intervals

---
+++ DAY01.cpp
@@ -0,0 +1,47 @@
+// Day 01
+// Problem: Overlapping Intervals
+// Platform: GeeksforGeeks POTD
+// Challenge: 60-Day DSA Challenge sponsored by NPCI
+// Date: 21/03/2025
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int overlapInt(vector<vector<int>>& arr) {
+        int n = arr.size();
+
+        vector<int> start(n), end(n);
+
+        for(int i = 0; i < n; i++) {
+            start[i] = arr[i][0];
+            end[i] = arr[i][1];
+        }
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int i = 0, j = 0;
+        int current = 0, maximum = 0;
+
+        while(i < n && j < n) {
+            if(start[i] <= end[j]) {
+                current++;
+                maximum = max(maximum, current);
+                i++;
+            } else {
+                current--;
+                j++;
+            }
+        }
+
+        return maximum;
+    }
+};
+
+int main() {
+    Solution sol;
+    // test your code here
+    return 0;
+}
